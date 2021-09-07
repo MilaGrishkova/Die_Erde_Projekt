374f389f2441489aa59d5fba381aa9c30903c4de
MilaGrishkova <49581816+MilaGrishkova@users.noreply.github.com>
2021-09-07 06:25:59 +0200

    Update Audio.cs

--- Audio.cs
+++ Audio.cs
@@ -23,7 +23,7 @@ public class Audio : MonoBehaviour
             {
                 Debug.Log("if1");
                 var selection = Hit.transform;
-                if (selection.CompareTag("Tag"))
+                if (selection.CompareTag("Sphere1"))
                 {
                     myAudioSource.clip = aClips[Random.Range(0, aClips.Length)];
                     myAudioSource.PlayOneShot (myAudioSource.clip);
