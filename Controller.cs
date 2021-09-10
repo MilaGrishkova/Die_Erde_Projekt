caca437675c2e92d6cf1cf63b5f030f2f44ab746
MilaGrishkova <49581816+MilaGrishkova@users.noreply.github.com>
2021-09-10 08:14:12 +0200

    Update Controller.cs

--- Controller.cs
+++ Controller.cs
@@ -8,7 +8,7 @@ public class Controller : MonoBehaviour
     public GameObject Sphere2;
     public GameObject ARCamera;
     public GameObject Target;
-    public soundeffector Soundeffector;
+    public soundeffector soundeffector;
     public AudioSource myAudioSource;
 
     void Start()
@@ -17,40 +17,40 @@ public class Controller : MonoBehaviour
         myAudioSource = GetComponent<AudioSource>();
     }
 
-    void Update ()
-
+    void Update()
     {
         if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
-           Debug.Log("Touch > 0");
-           Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-           RaycastHit Hit;
-           if (Physics.Raycast(ray, out Hit))
-              {
-               Debug.Log("if1");
-               var selection = Hit.transform;
-               if (selection.CompareTag("Sphere1"))
-                  {
-                      Destroy(Sphere1);
-                      Soundeffector.PlayBoomSound();
-                      Sphere2.gameObject.SetActive(true);
-                      Sphere2.transform.position = Vector3.zero;
-                      Target.GetComponent<AudioSource>().Play();
-                      Invoke("AudioSourceDestr", 10f);
-               }
+            Debug.Log("Touch > 0");
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit Hit;
+            if (Physics.Raycast(ray, out Hit))
+            {
+                Debug.Log("if1");
+                var selection = Hit.transform;
+                if (selection.CompareTag("Sphere1"))
+                {
+                    Destroy(Sphere1);
+                    soundeffector.PlayBoomSound();
+                    Sphere2.gameObject.SetActive(true);
+                    Sphere2.transform.position = Vector3.zero;
+                    Target.GetComponent<AudioSource>().Play();
+                    Invoke("AudioSourceDestr", 10f);
+                }
+            }
+
+            /* void Update()
+                 {
+                      if(Input.GetKey(KeyCode.Space))
+                      {
+                        ARCamera.GetComponent.<soundeffector>().enabled = false;
+                      }
+                 }*/
         }
 
-   /* void Update()
+        void AudioSourceDestr()
         {
-             if(Input.GetKey(KeyCode.Space))
-             {
-               ARCamera.GetComponent.<soundeffector>().enabled = false;
-             }
-        }*/
-    }
-
-    void AudioSourceDestr()
-    {
-        Destroy(myAudioSource);
+            Destroy(myAudioSource);
+        }
     }
 }
